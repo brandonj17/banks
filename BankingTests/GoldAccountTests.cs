5876dfebeadbfb355296ae3c40e2125c3878547d
brandonj17 <37629186+brandonj17@users.noreply.github.com>
2021-03-17 15:08:57 -0400

    Initialize loyalty-inheritance

---
+++ BankingTests/GoldAccountTests.cs
@@ -0,0 +1,31 @@
+﻿using BankingDomain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BankingTests
+{
+    public class GoldAccountTests
+    {
+        [Theory]
+        [InlineData(100, 10)]
+        public void GoldAccountsGetBonusOnDeposit(
+            decimal amountToDeposit,
+            decimal expectedBonus)
+        {
+            var account = new GoldAccount();
+            var openingBalance = account.GetBalance();
+
+
+            account.Deposit(amountToDeposit);
+
+            var expected = openingBalance + amountToDeposit + expectedBonus;
+
+            Assert.Equal(expected, account.GetBalance());
+           
+        }
+    }
+}
