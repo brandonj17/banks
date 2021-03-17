897218a8babf21729ee83452b13a00bbe82aa385
brandonj17 <37629186+brandonj17@users.noreply.github.com>
2021-03-17 15:08:57 -0400

    Initialize loyalty-flags

---
+++ BankingDomain/BankAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace BankingDomain
+{
+    public enum AccountType {  Standard, Gold }
+    public class BankAccount
+    {
+        private decimal _balance = 5000; // Class Variable "Field"
+        public AccountType AccountType = AccountType.Standard;
+        public decimal GetBalance()
+        {
+            return _balance;
+        }
+
+        public void Deposit(decimal amountToDeposit)
+        {
+            decimal bonus = 0;
+            if(AccountType == AccountType.Gold)
+            {
+                bonus = amountToDeposit * .10M;
+            }
+            _balance += amountToDeposit + bonus;
+        }
+
+        public void Withdraw(decimal amountToWithdraw)
+        {
+            _balance -= amountToWithdraw;
+        }
+    }
+}
---
+++ BankingTests/GoldCustomerTests.cs
@@ -0,0 +1,29 @@
+﻿using BankingDomain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BankingTests
+{
+    public class GoldCustomerTests
+    {
+        [Theory]
+        [InlineData(100, 10)]
+        public void GetABonusOnDeposits(decimal amountToDeposit, decimal expectedBonus)
+        {
+            // Given
+            var account = new BankAccount();
+            var openingBalance = account.GetBalance();
+            account.AccountType = AccountType.Gold;
+            // When
+            account.Deposit(amountToDeposit);
+
+            // Then
+            var expected = openingBalance + amountToDeposit + expectedBonus;
+            Assert.Equal(expected, account.GetBalance());
+        }
+    }
+}
